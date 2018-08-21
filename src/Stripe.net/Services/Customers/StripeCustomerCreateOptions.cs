492eee4f1a5c4cd740f2231d8a96f26ced51455e
Remi Jannel <remi@stripe.com>
2018-08-21 17:48:24 -0400

    Rename SourceCard to CardCreateNestedOptions

--- src/Stripe.net/Services/Customers/StripeCustomerCreateOptions.cs
+++ src/Stripe.net/Services/Customers/StripeCustomerCreateOptions.cs
@@ -41,7 +41,7 @@
         public string SourceToken { get; set; }
 
         [JsonProperty("source")]
-        public SourceCard SourceCard { get; set; }
+        public CardCreateNestedOptions SourceCard { get; set; }
 
         [JsonProperty("tax_percent")]
         public decimal? TaxPercent { get; set; }
